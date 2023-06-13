0458b6fd2fc01d665f5157d6adf598c58d8d50b7
karie <kmoorman@ucmerced.edu>
2023-06-13 09:08:40 -0700

    Update __init__.py

--- __scripts/reddit_scraper/__init__.py
+++ __scripts/reddit_scraper/__init__.py
@@ -1,10 +1,1 @@
-#import os
-#import sys
-#
-#sys.path.insert(0, 'reddit/__scripts/reddit_scraper')
-#sys.path.insert(0, 'reddit/__scripts')
-#
-#
-#__all__ = ['subreddit_lists', 'subreddit_scraper','subreddit_search_scraper', 'reddit-user-activity-scraper', 'subreddit-user-activity-scraper', 'credentials']
-#
-#from reddit_scraper import *
+
