914e137ce17e389fd7b353ead57c20e1a5b45067
ariscae <111535040+ariscae@users.noreply.github.com>
2023-07-22 20:18:36 -0700

    access

--- reddit/__scripts/reddit_sql/create_access_privileges.sql
+++ reddit/__scripts/reddit_sql/create_access_privileges.sql
@@ -15,13 +15,21 @@ CREATE ROLE <username_c> inherit LOGIN PASSWORD '<password>' VALID UNTIL 'timest
 CREATE ROLE <username_d> inherit LOGIN PASSWORD '<password>' VALID UNTIL 'timestamp';
 CREATE ROLE <username_e> inherit LOGIN PASSWORD '<password>' VALID UNTIL 'timestamp';
 
-/* --- GRANT permissions on Database Schema & Tables to Groups & User(s) --- */ 
+/* --- Grant privileges on Database Schema & Tables to Groups & User(s) --- */ 
 
+/* --- MySQL --- */
 GRANT ALL ON reddit.* TO administrator;
-GRANT CREATEDB ON reddit.* TO dba;
+GRANT CREATE TO dba;
 GRANT SELECT ON reddit.* TO reader;
 GRANT INSERT, UPDATE, DELETE, SELECT ON reddit.* TO dev_api;
 GRANT INSERT, SELECT ON reddit.* TO api;
+
+/* --- PostgreSQL --- */
+GRANT ALL ON ALL TABLES IN SCHEMA reddit TO administrator GRANTED BY <rolename>;
+GRANT CREATEDB  TO dba;
+GRANT SELECT ON ALL TABLES IN SCHEMA reddit TO reader;
+GRANT INSERT, UPDATE, DELETE, SELECT ON ALL TABLES IN SCHEMA reddit TO dev_api;
+GRANT INSERT, SELECT ON ALL TABLES IN SCHEMA reddit TO api;
 
 /* --- Assign User(s) to Group(s) --- */
 
@@ -30,3 +38,14 @@ GRANT dba TO <username_b>;
 GRANT reader TO <username_c>;
 GRANT dev_api TO <username_d>;
 GRANT api TO <username_e>;
+
+
+/* --- Show Privileges for User(s) and Groups --- */ 
+
+SHOW GRANTS FOR administrator;
+SHOW GRANTS FOR <username_c>;
+
+
+/* --- Revoke Group Privileges to Users --- */
+
+REVOKE reader FROM <username_c>;
