95076aee165a322ed22700218ecde2c6074f134f
ariscae <111535040+ariscae@users.noreply.github.com>
2023-07-24 13:41:56 -0700

    update

--- reddit/__scripts/reddit_scraper/subreddit_search_scraper.py
+++ reddit/__scripts/reddit_scraper/subreddit_search_scraper.py
@@ -9,7 +9,7 @@ import requests
 import praw
 
 ## add credentials.py script to .gitignore list to keep personal keys safe. ##
-from reddit_scraper.credentials import *
+from credentials import *
 
 class SubredditSearchScraper:
     def __init__(self, subreddits, category, sep='tab', output_format='csv'):
